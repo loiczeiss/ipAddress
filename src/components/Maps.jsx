310c8d02031499806e11c4a982d46ae7acdd7efa
Loïc Zeiss <loiczeiss@gmail.com>
2024-04-14 17:54:22 +0200

    correcting map behavior when coord changes

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,18 +1,25 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
-import "../maps.css"
-export default function Maps({coord, passedData}) {
-  const {  location } = passedData;
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import "../maps.css";
+
+export default function Maps({ coord, passedData }) {
+  const { location } = passedData;
+  const [mapKey, setMapKey] = useState(0);
+
+  useEffect(() => {
+    // Update the key when coord changes
+    setMapKey(prevKey => prevKey + 1);
+  }, [coord]);
+
   return (
-<MapContainer  center={coord} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={coord}>
-{location &&     <Popup>
-  {location.city} <br /> {location.region}
-    </Popup>}
-  </Marker>
-</MapContainer>
-  )
+    <MapContainer key={mapKey} center={coord} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={coord}>
+        {location && <Popup>{location.city} <br /> {location.region}</Popup>}
+      </Marker>
+    </MapContainer>
+  );
 }
